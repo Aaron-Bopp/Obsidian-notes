c376c23fb5e45e4c54fa5542ac042a795c504968
Aaron Bopp <s533127@nwmissouri.edu>
2021-05-09 20:12:28 -0500

    add file reading

--- tharoline-testing/post-proccessing.py
+++ tharoline-testing/post-proccessing.py
@@ -2,14 +2,28 @@
 import json
 import os
 with open('entities.json') as f:
-    entities = json.load(f)  
+    entities = json.load(f)
 
 # %%
+
+
 def load_notes():
+    notes = {}
     with os.scandir() as vault:
-        for folder in vault:
-            if folder.is_dir() and folder.name != ".obsidian":
-                with os.scandir(folder.path) as vault_folder:
+        for entry in vault:
+            if entry.is_dir() and entry.name != ".obsidian":
+                with os.scandir(entry.path) as vault_folder:
                     for note in vault_folder:
                         if note.name.endswith(".md"):
-                            
+                            name = note.name.split(".")[0]
+                            with open(note.path) as text:
+                                lines = text.readlines()
+                                notes.setdefault(name, lines)
+            if entry.is_file():
+                if entry.name.endswith(".md"):
+                    name = entry.name.split(".")[0]
+                    with open(entry.path) as text:
+                        lines = text.readlines()
+                        notes.setdefault(name, lines)
+    return notes
+# %%
